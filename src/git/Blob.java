d2b10a8877ea7247c7dcf916c144407812855fed
Henry <henrydorff@gmail.com>
2022-09-12 07:32:35 -0700

    Added Comments to make it easier to read

--- src/git/Blob.java
+++ src/git/Blob.java
@@ -9,21 +9,21 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 public class Blob {
-	private String SHAString;
+	private String SHAString; 
 	public Blob (String fileName) throws IOException {
 		BufferedReader br = new BufferedReader (new FileReader (fileName));
 		StringBuilder temp = new StringBuilder();
 		while (br.ready()) {
 			char c = (char) br.read();
 			temp.append(c);
-		}
-		String fileString = new String (temp.toString());
-		String SHAString = new String (SHA1.encryptThisString(fileString));
+		} // copied entire contents of a file onto a StringBuilder
+		String fileString = new String (temp.toString()); // fileString is string with entire contents of the file on it
+		String SHAString = new String (SHA1.encryptThisString(fileString)); // SHAString is String with a SHAString of the contents of fileString
 		this.SHAString = SHAString;
 		
-		File newBlob = new File ("Testing/objects/"+ SHAString + ".txt");
-		FileWriter myWriter = new FileWriter (newBlob);
-		myWriter.write(fileString);
+		File newBlob = new File ("Testing/objects/"+ SHAString + ".txt"); // new blob created in Testing/objects (objects folder will be created in Index class) with its name being the SHAString
+		FileWriter myWriter = new FileWriter (newBlob); // connects FileWriter to the new file
+		myWriter.write(fileString); // writes the contents of the original file onto this new file
 		myWriter.close();;
 		
 	}
--- src/git/Index.java
+++ src/git/Index.java
@@ -8,11 +8,16 @@ import java.util.HashMap;
 import java.util.Scanner;
 
 public class Index {
+	// iWriter will only be used in start(), to make the index file a .txt file
+	// don't worry about it otherwise
 	FileWriter iWriter;
+	// blobStorage stores a key (fileName) and a value (its SHAString)
 	private HashMap<String, String> blobStorage = new HashMap<String, String>();
+	
 	public Index () {	
 	}
 	
+	//creating index file, making it a .txt; creating objects, making it a folder
 	public void start () throws IOException {
 		File index = new File ("Testing/index.txt");
 		iWriter =  new FileWriter (index);
@@ -21,7 +26,9 @@ public class Index {
 	}
 	
 	public void add (String fileName) throws IOException {
+		// creating StringBuilder that will become the string contained in the index.txt file
 		StringBuilder newString = new StringBuilder ();
+		// using a scanner and while loop, inputting everything from index.txt into StringBuilder
 		Scanner fileCopier = new Scanner (new File ("Testing/index.txt"));
 		while (fileCopier.hasNextLine()) {
 			String newLine = fileCopier.nextLine();
@@ -30,30 +37,33 @@ public class Index {
 			newString.append("\n");
 		}
 		fileCopier.close();
+		// Now adding the entry for the new file into index.txt
 		newString.append(fileName + " : ");
 		Blob newBlob = new Blob (fileName);
-		blobStorage.putIfAbsent(fileName, newBlob.getSHAString());
-		System.out.println (blobStorage.get(fileName));
+		blobStorage.putIfAbsent(fileName, newBlob.getSHAString()); //adding to HashMap for use later in remove
+		System.out.println (blobStorage.get(fileName)); // makes testing easier
 		newString.append("" + blobStorage.get(fileName) + "\n");
-		FileWriter indexWriter = new FileWriter ("Testing/index.txt");
-		indexWriter.write(newString.toString());
+		FileWriter indexWriter = new FileWriter ("Testing/index.txt"); // assigning FileWriter to index.txt
+		indexWriter.write(newString.toString()); // after finally completing the StringBuilder, it is being written to index.txt
 		indexWriter.close();
 	}
 	
 	public void remove (String fileName) throws IOException {
-		int numOfCharsInFileName = fileName.length();
+		int numOfCharsInFileName = fileName.length(); // keeping track of this so that we can search only the first part of a blob's entry in index.txt
+		// for example, in Testing/sample.txt, we just want to read "sample.txt", so it's good to know that "sample.txt" is 10 chars
 		File toDelete = new File ("Testing/objects/" + blobStorage.get("Testing/" + fileName) + ".txt");
-		toDelete.delete();
-		Scanner fileScanner = new Scanner (new File ("Testing/index.txt"));
-		StringBuilder newString = new StringBuilder ();
+		toDelete.delete();// deletes the desired file in objects folder
+		Scanner fileScanner = new Scanner (new File ("Testing/index.txt")); // will scan through index.txt
+		StringBuilder newString = new StringBuilder (); // new StringBuilder will eventually replace whatever text is currently in index.txt
 		while (fileScanner.hasNextLine()) {
 			String newLine = fileScanner.nextLine();
-			if (!(newLine.substring(8, 8 + numOfCharsInFileName).equals(fileName))) {
+			if (!(newLine.substring(8, 8 + numOfCharsInFileName).equals(fileName))) { // tests if the newLine is the one we want to remove. If it is not, it will be added to StringBuilder
+				// thus resulting in everything but the object we want to delete being in the StringBuilder
 				newString.append(newLine);
 				newString.append("\n");
 			}
 		}
-		FileWriter indexWriter = new FileWriter ("Testing/index.txt");
+		FileWriter indexWriter = new FileWriter ("Testing/index.txt"); // finally writes StringBuilder to index.txt
 		indexWriter.write(newString.toString());
 		indexWriter.close();
 	}
